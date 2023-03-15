6b6e01a320e9ec3e974be91bab381e2c7a68361b
Wali Muhammad <luckywali0986@gmail.com>
2023-03-15 12:13:21 +0500

    modified library for the decorate class

--- library.rb
+++ library.rb
@@ -1,24 +1,28 @@
-class Person
-  attr_accessor :name, :age, :id
+require './modules/nameable'
+class Person < Nameable
+  attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  private
-
-  def of_age
-    @age >= 18
-  end
-
-  public
-
   def can_use_services
     return unless @age >= 18 || parent_permission
 
     true
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age
+    @age >= 18
   end
 end
