24431ac5f76112221795c53e33c1e546b78424e8
Wali Muhammad <luckywali0986@gmail.com>
2023-03-16 17:43:10 +0500

    setup associates and links in the student class

--- modules/student.rb
+++ modules/student.rb
@@ -2,12 +2,19 @@ require_relative '../person'
 require_relative './classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(name, age, parent_permission, classroom)
     super(name, age, parent_permission)
     @classroom = classroom
   end
 
-  def play_hooky()
+  def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def add_to_classroom(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 end
