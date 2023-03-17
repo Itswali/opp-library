f7130b5b35f716ad1a65088d11a749717d7a42af
Wali Muhammad <luckywali0986@gmail.com>
2023-03-17 18:37:30 +0500

    add parent permission on arguments

--- modules/student.rb
+++ modules/student.rb
@@ -2,10 +2,11 @@ require_relative '../person'
 require_relative './classroom'
 
 class Student < Person
+  attr_accessor :name, :age, :parent_permission
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
