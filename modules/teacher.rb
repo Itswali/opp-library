5361fed58cc12eccab0aa2f33169616b13cd8b71
Wali Muhammad <luckywali0986@gmail.com>
2023-03-17 18:38:23 +0500

    remove code on teacher permission due to unmentioned arguements

--- modules/teacher.rb
+++ modules/teacher.rb
@@ -1,11 +1,16 @@
 require_relative '../person'
 class Teacher < Person
-  def initialize(age, specialization, name = 'unknown')
-    super
+  attr_accessor :specialization
+  attr_reader :parent_permission
+
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(age, name = 'Unkown', specialization = nil, parent_permission = true)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
-  def can_use_service?
+  # rubocop:enable Style/OptionalBooleanParameter
+  def can_use_services
     true
   end
 end
--- person.rb
+++ person.rb
@@ -2,34 +2,38 @@ require_relative './modules/nameable'
 require_relative './modules/rental'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals, :book, :date
+  attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
-    @id = SecureRandom.uuid
+    @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
 
+  # rubocop:enable Style/OptionalBooleanParameter
+
+  def can_use_services
+    return unless @age >= 18 || @parent_permission
+
+    true
+  end
+
   def correct_name
     @name
   end
 
-  def can_use_services?
-    of_age? || @parent_permission == true
-  end
-
-  def add_rentals(rental)
-    rentals.push(rental)
-    rental.person = self
-  end
-
   private
 
-  def of_age?
+  def of_age
     @age >= 18
+  end
+
+  def add_rentals(book, date)
+    @rentals << Rental.new(date, book, self)
   end
 end
