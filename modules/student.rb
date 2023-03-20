49e675c82f5c60e42307c34d9aa40acaea191ddf
Juanpa8830 <jp3_4@hotmail.com>
2023-03-20 14:18:59 -0500

    Refactor student class

--- modules/student.rb
+++ modules/student.rb
@@ -2,20 +2,19 @@ require_relative '../person'
 require_relative './classroom'
 
 class Student < Person
-  attr_accessor :name, :age, :parent_permission
-  attr_reader :classroom
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
-    @classroom = classroom
-  end
-
-  def play_hooky
-    '¯(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-  end
+  attr_accessor :classroom
+  attr_reader :parent_permission
+# rubocop:disable Style/OptionalBooleanParameter
+def initialize(age, name = 'Unkown', parent_permission = true)
+  super(age, name, parent_permission)
+  @classroom = classroom
+end
+ # rubocop:enable Style/OptionalBooleanParameter
+ def play_hooky
+  '¯\(ツ)/¯'
+ end
+ def add_to_classroom(classroom)
+  @classroom = classroom
+  classroom.students.push(self) unless classroom.students.include?(self)
+ end
 end
