97bd9778a319f574c47e07ca9e9fa4f69ba131b8
Wali Muhammad <luckywali0986@gmail.com>
2023-03-20 21:15:41 +0500

    fix linter error

--- modules/student.rb
+++ modules/student.rb
@@ -5,7 +5,7 @@ class Student < Person
   attr_accessor :name, :age, :parent_permission
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
