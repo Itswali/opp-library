307d75634e568b426c3d5bc6487d7e925a8c1a1e
Juanpa8830 <jp3_4@hotmail.com>
2023-03-20 14:20:54 -0500

    Fix linter errors

--- modules/student.rb
+++ modules/student.rb
@@ -4,17 +4,20 @@ require_relative './classroom'
 class Student < Person
   attr_accessor :classroom
   attr_reader :parent_permission
-# rubocop:disable Style/OptionalBooleanParameter
-def initialize(age, name = 'Unkown', parent_permission = true)
-  super(age, name, parent_permission)
-  @classroom = classroom
-end
- # rubocop:enable Style/OptionalBooleanParameter
- def play_hooky
-  '¯\(ツ)/¯'
- end
- def add_to_classroom(classroom)
-  @classroom = classroom
-  classroom.students.push(self) unless classroom.students.include?(self)
- end
+
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(age, name = 'Unkown', parent_permission = true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  # rubocop:enable Style/OptionalBooleanParameter
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+
+  def add_to_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
